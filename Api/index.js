b3fe7b9549106630a6fed951e9df75e5e021ea91
Gabix008 <gabriellysantana583@gmail.com>
2024-09-16 07:47:24 -0300

    Melhorias

--- Api/index.js
+++ Api/index.js
@@ -10,6 +10,8 @@ const cors = require('cors');
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 const ElevenLabs = require("elevenlabs-node");
 const deepl = require('deepl-node');
+const ffmpeg = require('fluent-ffmpeg');
+
 //const upload = multer({ dest: 'uploads/' });
 
 //Credenciais Speech to text(IBM)
@@ -66,64 +68,84 @@ app.post('/recognize', async (req, res) => {
     }
 
     const audioFilePath = `${__dirname}/media/${req.file.filename}`;// Constrói o caminho completo do arquivo de áudio
-    const params = {
-      audio: fs.createReadStream(audioFilePath),
-      contentType: 'audio/mp3',
-      endOfPhraseSilenceTime: 20,
-      smartFormatting: true,
-    }
+    const convertedFilePath = `${__dirname}/media/${Date.now()}.wav`;
 
-    const speechToText = new SpeechToTextV1({
-      authenticator: authenticator,
-      serviceUrl: url,
-    })
+    ffmpeg(audioFilePath)
+      .output(convertedFilePath)
+      .audioCodec('pcm_s16le')
+      .format('wav')
+      .on('end', () => {
+        const params = {
+          audio: fs.createReadStream(convertedFilePath),
+          contentType: 'audio/wav',
+          endOfPhraseSilenceTime: 20,
+          smartFormatting: true,
+        }
+        const speechToText = new SpeechToTextV1({
+          authenticator: authenticator,
+          serviceUrl: url,
+        })
+        const languageTranslator = new LanguageTranslatorV3({
+          version: '2018-05-01',
+          authenticator: new IamAuthenticator({
+            apikey: '51nzSU6w3RA6eUCkvFaNjR6I8JcoufO99NljNzujLoVz',
+          }),
+          headers: {
+            'X-Watson-Learning-Opt-Out': 'true'
+          },
+          serviceUrl: 'https://api.au-syd.language-translator.watson.cloud.ibm.com/instances/eb96f779-fff6-43b0-8e51-7ea40e468404',
+        });
+        speechToText.recognize(params)
+          .then(response => {
+            const translateParams = {
+              text: response.result.results[0].alternatives[0].transcript,
+              modelId: 'en-pt',
+            };
+
+            languageTranslator.translate(translateParams)
+              .then(async (response) => {
 
 
+                voice.textToSpeechStream({
+                  fileName: "audio.mp3",
+                  textInput: response.result.translations[0].translation,
+                  voiceId: 'QJd9SLe6MVCdF6DR0EAu',
+                  stability: 0.54,
+                  similarityBoost: 0.36,
+                  modelId: "eleven_multilingual_v2",
+                  responseType: "stream",
+                  speakerBoost: true
+                }).then((audio) => {
+                  const tempFile = `${__dirname}/public/audio.mp3`
+                  audio.pipe(fs.createWriteStream(tempFile))
+                  res.status(200).send(tempFile)
+                  console.log('O aúdio foi criado!',)
 
-    const languageTranslator = new LanguageTranslatorV3({
-      version: '2018-05-01',
-      authenticator: new IamAuthenticator({
-        apikey: '51nzSU6w3RA6eUCkvFaNjR6I8JcoufO99NljNzujLoVz',
-      }),
-      headers: {
-        'X-Watson-Learning-Opt-Out': 'true'
-      },
-      serviceUrl: 'https://api.au-syd.language-translator.watson.cloud.ibm.com/instances/eb96f779-fff6-43b0-8e51-7ea40e468404',
-    });
-
-    speechToText.recognize(params)
-      .then(response => {
-        const translateParams = {
-          text: response.result.results[0].alternatives[0].transcript,
-          modelId: 'en-pt',
-        };
-
-        languageTranslator.translate(translateParams)
-          .then(async (response) => {
-
-
-            const voiceResponse = voice.textToSpeechStream({
-              fileName: "audio.mp3",
-              textInput: response.result.translations[0].translation,
-              voiceId: 'QJd9SLe6MVCdF6DR0EAu',
-              stability: 0.54,
-              similarityBoost: 0.36,
-              modelId: "eleven_multilingual_v2",
-              responseType: "stream",
-              speakerBoost: true
-            }).then((audio) => {
-              const tempFile = `${__dirname}/public/audio.mp3`
-              audio.pipe(fs.createWriteStream(tempFile))
-              res.status(200).send(tempFile)
-              console.log('O aúdio foi criado!',)
-
-            });
+                })
+                  .catch(err => {
+                    res.status(500).json(err);
+                    console.log('Erro:', err);
+                  });
+              })
           })
+          .catch(err => {
+            res.status(500).json(err);
+            console.log('Erro:', err);
+          });
       })
-      .catch(err => {
-        res.status(500).json(err);
-        console.log('Erro:', err);
-      });
+      .on('error', err => {
+        console.log('Erro na conversão de áudio:', err);
+        res.status(500).send('Erro ao converter o arquivo de áudio.');
+      })
+      .run();
+    console.log(req.file)
+
+
+
+
+
+
+
   })
 });
 
